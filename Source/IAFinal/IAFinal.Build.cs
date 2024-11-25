8cea646c66aa1a13ffafd2550ede8875eebc09c7
Titouan Fréquelin <136023547+tfrequelt@users.noreply.github.com>
2024-11-24 23:24:36 -0500

    procedural step 1 cleanup world

---
+++ Source/IAFinal/IAFinal.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class IAFinal : ModuleRules
+{
+	public IAFinal(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
